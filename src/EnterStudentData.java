291b3f62348567cbfd900c1302f5582c940c7738
Paul Franklin <paul.franklin@csuglobal.edu>
2024-05-09 21:37:57 -0400

    taking user input

--- src/EnterStudentData.java
+++ src/EnterStudentData.java
@@ -1,2 +1,10 @@
 public class EnterStudentData {
+    private String name;
+    private String address;
+    private double GPA;
+    public EnterStudentData(String name, String address, double GPA){
+        this.name = name;
+        this.address = address;
+        this.GPA = GPA;
+    }
 }
